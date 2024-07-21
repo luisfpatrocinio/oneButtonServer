d7b2e63b5e96ac6ac834306433885989a7431996
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-07-21 04:52:44 -0300

    feat: ports adjusts and page status display

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     const connectButton = document.getElementById('connectButton');
+    const statusMessage = document.getElementById('statusMessage');
 
-    const ws = new WebSocket("wss://onebuttonserver.onrender.com:5569");
+    const ws = new WebSocket('wss://onebuttonserver.onrender.com:10000');
 
     ws.onopen = () => {
         console.log('Connected to WebSocket server');
+        statusMessage.textContent = "Conectado!";
         connectButton.classList.remove('hidden');
     };
 
@@ -12,11 +14,14 @@ document.addEventListener('DOMContentLoaded', (event) => {
         console.log('Message from server:', event.data);
     };
 
-    ws.onclose = () => {
+    ws.onclose = (event) => {
         console.log('Disconnected from WebSocket server');
+        console.log('Code:', event.code, 'Reason:', event.reason);
+        statusMessage.textContent = "Desconectado.";
     };
 
     ws.onerror = (error) => {
         console.error('WebSocket error:', error);
+        statusMessage.textContent = "Erro de conexão.";
     };
 });
